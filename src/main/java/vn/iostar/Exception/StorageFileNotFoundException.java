a956f6394643c135cc52d6404c1ef2fa427e4d06
TaNghiaNhan <164967863+TaNghiaNhan@users.noreply.github.com>
2024-10-29 11:27:46 +0700

    CRUD

--- src/main/java/vn/iostar/Exception/StorageFileNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package vn.iostar.Exception;
-
-public class StorageFileNotFoundException extends StorageException{
-    private static final long serialVersionUID = 1L;
-    public StorageFileNotFoundException(String message) {
-        super(message);
-    }
-}
